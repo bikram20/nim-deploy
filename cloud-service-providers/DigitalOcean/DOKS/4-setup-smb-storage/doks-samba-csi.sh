ee4cb48d58fc775fa20d47f1635517d3f4c299b9
jkpe <16779171+jkpe@users.noreply.github.com>
2024-09-09 11:57:29 +0100

    chore: Comment out some commands to avoid confusion in doks-samba-csi.sh

--- cloud-service-providers/DigitalOcean/DOKS/4-setup-smb-storage/doks-samba-csi.sh
+++ cloud-service-providers/DigitalOcean/DOKS/4-setup-smb-storage/doks-samba-csi.sh
@@ -20,18 +20,18 @@ kubectl get storageclass smb
 
 # If pods do not become active and PVC is still not bound, then check the SMB controller pod log. 
 # Most likely culprit is either you did not specify credentials or the path in the storageclass is incorrect.
-kubectl exec smb-test-pod-1 -- cat /mnt/data/testfile2
-kubectl exec smb-test-pod-1 -- cat /mnt/data/testfile1
 
-kubectl delete -f Scripts_Artifacts/smb-test.yaml
+# kubectl exec smb-test-pod-1 -- cat /mnt/data/testfile2
+# kubectl exec smb-test-pod-1 -- cat /mnt/data/testfile1
+
+# kubectl delete -f Scripts_Artifacts/smb-test.yaml
 
 
 # Read write performance test
-While large model files will be sequential read-write, let us measure both sequential and random access performance.
-kubectl create -f fio-benchmark.yaml
-kubectl logs fio-benchmark
+# While large model files will be sequential read-write, let us measure both sequential and random access performance.
+# kubectl create -f fio-benchmark.yaml
+# kubectl logs fio-benchmark
 
-kubectl delete -f fio-benchmark.yaml
+# kubectl delete -f fio-benchmark.yaml
 
-For a basic 1cpu/2gb droplet with 250GB DO volume, we get around ~218MB/s of sequential read/write performance. This should be good enough for our use case (storing model files).
-
+# For a basic 1cpu/2gb droplet with 250GB DO volume, we get around ~218MB/s of sequential read/write performance. This should be good enough for our use case (storing model files).
